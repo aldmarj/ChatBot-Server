cc3ed81729c90836a82a48ee58065e17f733c458
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-03-13 20:41:47 +0000

    added more build scripts

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ const processMessage = require('./process-message');
 
 const app = express();
 
-var corsOptions = {
-    origin: 'https://brave-galileo-6773b0.netlify.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
 app.use(helmet());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.post('/chat', (req, res) => {
     const { message } = req.body;
