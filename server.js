cefceadf68d32875aaaaf2977a0028bae128e273
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-03-13 13:56:42 +0000

    Added pm2 with build script

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require('dotenv').config({ path: 'variables.env' });
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var helmet = require('helmet');
 const cors = require('cors');
+var compression = require('compression');
 const processMessage = require('./process-message');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
